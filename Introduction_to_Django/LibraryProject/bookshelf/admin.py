dd72bf68de915953f10ea9dff9d1f9f9b75c1a14
ABOT-DK <davidkabotsi@gmail.com>
2025-07-12 17:45:49 +0000

    create user and fields in admin dashboard

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Book
+
+#Customise for search, list and filter capabilitites
+
+class BookAdmin(admin.ModelAdmin):
+    # Show these fields as columns in the list view
+    list_display = ('title', 'author', 'publication_year')
+    
+    # Add filters in the right sidebar for quick filtering
+    list_filter = ('author', 'publication_year')
+    
+    # Include a search box to search by title and author fields
+    search_fields = ('title', 'author')
+
 
 # Register your models here.
+admin.site.register(Book, BookAdmin)
